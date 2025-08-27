0841cad032052b4be9761d541f9a006794c5934e
Raidel <yoshilol0526@gmail.com>
2025-08-27 17:21:23 -0300

    Back to sync db connection

--- backend/src/core/db.py
+++ backend/src/core/db.py
@@ -1,17 +1,15 @@
-from typing import AsyncGenerator
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from typing import Generator
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 from src.core.config import settings
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
+def get_db() -> Generator[Session, None]:
     """
     Yields a database session for use in async contexts.
     """
-    engine = create_async_engine(settings.DATABASE_URL, echo=True)
-    async_session = sessionmaker(
-        bind=engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
+    engine = create_engine(settings.DATABASE_URL, echo=True)
+    session = sessionmaker(bind=engine, expire_on_commit=False)
+    with session() as session:
         yield session
-    await engine.dispose()
+    engine.dispose()
--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,7 +1,7 @@
-from typing import Annotated, AsyncGenerator
+from typing import Annotated, Generator
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 
 from src.core.db import get_db
 
-DatabaseSession = Annotated[AsyncGenerator[AsyncSession, None], Depends(get_db)]
+DatabaseSession = Annotated[Generator[Session, None], Depends(get_db)]
