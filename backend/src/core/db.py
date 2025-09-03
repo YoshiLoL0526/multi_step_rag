9161f6274c945b7d2cb6ba1d6e807e76092b8b2c
Raidel <yoshilol0526@gmail.com>
2025-09-02 22:44:33 -0300

    Disable SQLAlchemy echo for database engine to reduce log verbosity

--- backend/src/core/db.py
+++ backend/src/core/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from src.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=False)
 
 
 def create_tables():
